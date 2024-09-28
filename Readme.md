4e53cdcb759e79c0b9f8f6dd52d5c1da34fca80b
Darshit Limbad <darshitlimbad555@gmail.com>
2024-09-29 01:30:56 +0530

    ADDED LICENCE

--- Readme.md
+++ Readme.md
@@ -62,6 +62,26 @@ To build the executable file using cxfreeze, follow these steps:
 
 The executable will be available in the `dist` folder.
 
-## License
+## `License`:
 
-IDK
+    YouTube Video Downloader 2.0 - Youtube Video downloader in Python
+    Copyright (C) 2024  Darshit Limbad
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## `Contact`
+
+For further information or support, please reach out at:
+- **Email:** darshitlimbad+git@example.com
+- **LinkedIn:**
+https://www.linkedin.com/in/darshit-limbad/
--- main.py
+++ main.py
@@ -3,7 +3,33 @@ import yt_dlp
 from module.download import *
 from module.formatting import print_colored_text
 
+def print_license():
+    """Prints the license information."""
+    license_text = """
+        YouTube Video Downloader 2.0 - Youtube Video downloader in Python
+        Copyright (C) 2024  Darshit Limbad
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact
+        For further information or support, please reach out at:
+        - Email: darshitlimbad+git@example.com
+        - LinkedIn: https://www.linkedin.com/in/darshit-limbad/
+    """
+    print(license_text)
+
 def main():
+    print_license()
     print("-" * 100)
     
     # Display options to the user
